215724fcb39d6f558f90387af7ad27ac699775fc
shika1015 <shika@wayfarer-group.com>
2026-03-13 17:30:16 +0900

    Update page.js

--- page.js
+++ page.js
@@ -1,8 +1,6 @@
 'use client'
 import dynamic from 'next/dynamic'
-
-const App = dynamic(() => import('../../components/App'), { ssr: false })
-
+const App = dynamic(() => import('../../../components/App'), { ssr: false })
 export default function Page() {
   return <App />
 }
